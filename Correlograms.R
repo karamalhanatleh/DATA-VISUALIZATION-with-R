64a298dc1b5df70be07bd6a8391ea8436a2e1226
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2024-04-17 15:55:29 +0300

    Add files via upload

---
+++ Correlograms.R
@@ -0,0 +1,50 @@
+##########################
+#         Correlograms 
+#########################
+
+#load libraray
+library(ggcorrplot)
+
+
+# Compute a correlation matrix
+data(mtcars)
+corr<- round(cor(mtcars),1)
+head(corr[,1:6])
+
+
+# Compute a matrix of correlation p-values
+p.mat <-cor_pmat(mtcars)
+head(p.mat[,1:4])
+
+
+##Correlation matrix visualization
+
+# Visualize the correlation matrix
+
+ggcorrplot(corr) # method = "square" (default)
+
+
+ggcorrplot(corr , method = 'circle')
+
+# Reordering the correlation matrix
+ggcorrplot(corr , hc.order = TRUE)
+
+
+
+
+
+#Types of correlogram layout
+ggcorrplot(corr ,hc.order=TRUE , type = 'upper')
+
+ggcorrplot(corr , hc.order = TRUE , type='lower' )
+
+
+
+
+
+# Change colors and theme
+
+
+
+
+
